f900cee5a16e568d52bcbf4fa2ba1dbaadb55fe9
Joey C Arrigo <jca160030@cslinux1.utdallas.edu>
2018-11-02 14:35:58 -0500

    4.3 completed

--- myprog1.cpp
+++ myprog1.cpp
@@ -1,5 +1,12 @@
-#include <sys/stat.h>
+#include <fcntl.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 int main(){
-  mkdir(./dir1,700); 
+  mkdir("./dir1",0700);
+  creat("./dir1/file10",0700);
+  mkdir("./dir2",0700);
+  creat("./dir2/file20",0700);
+  creat("./file1",0700);
+  symlink("./dir2/file20","./link1");
 }
