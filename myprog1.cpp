6caf528e946bc2c15ab0f02cabbf61fc8d42bd15
Joey C Arrigo <jca160030@cslinux1.utdallas.edu>
2018-11-04 13:54:10 -0600

    4.4 Completed

--- myprog1.cpp
+++ myprog1.cpp
@@ -4,9 +4,12 @@
 #include <unistd.h>
 int main(){
   mkdir("./dir1",0700);
-  creat("./dir1/file10",0700);
+  int filedesc = open("./dir1/file10",O_CREAT,0700);
+  close(filedesc);
   mkdir("./dir2",0700);
-  creat("./dir2/file20",0700);
-  creat("./file1",0700);
+  filedesc = open("./dir2/file20",O_CREAT,0700);
+  close(filedesc);
+  filedesc=open("./file1",O_CREAT,0700);
+  close(filedesc);
   symlink("./dir2/file20","./link1");
 }
